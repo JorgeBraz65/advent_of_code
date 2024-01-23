b172756a177c13711913e889df67cc6d085e8453
Jorge Braz <jlbmesquita@gmail.com>
2024-01-22 21:42:46 -0300

    Adiciona a solução para a parte 1 do Dia 2

---
+++ 2015/Day_2/part_1.py
@@ -0,0 +1,18 @@
+from pathlib import Path
+
+PATH = Path(__file__).resolve().parent / "part_1.txt"
+
+with PATH.open("r") as file:
+    entrada = file.readlines()
+
+area_total = 0
+
+
+for linha in entrada:
+    dimensoes = linha.split("x")
+    length, width, height = map(int, dimensoes)
+    lados = [length*width, width*height, height*length]
+
+    area_total += 2*lados[0] + 2*lados[1] + 2*lados[2] + min(lados[0], lados[1], lados[2])
+
+print("A área total necessária é:", area_total)
